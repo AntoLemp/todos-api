486afe19d320dc377050a44a6e2fb9d7da75a759
AntoLemp <160651031+AntoLemp@users.noreply.github.com>
2026-02-18 13:10:18 +0200

    Add Todo and Item resource management with model associations, controllers, routes, and test coverage.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,14 @@
-# require database cleaner at the top level
-require 'database_cleaner'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
 
-# [...]
-# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+# Manually require these to ensure they are loaded before configuration
+require 'database_cleaner/active_record'
+require 'shoulda/matchers'
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+# Shoulda Matchers config
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
@@ -10,23 +16,35 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-# [...]
 RSpec.configure do |config|
-  # [...]
-  # add `FactoryBot` methods
+  # Include FactoryBot Methods
   config.include FactoryBot::Syntax::Methods
 
-  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+  # DatabaseCleaner Configuration
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
+    # Use 'deletion' or 'truncation' to reset the DB state once at the start
+    DatabaseCleaner.clean_with(:deletion)
+  end
+
+  config.before(:each) do
+    # Standard transaction strategy for speed
     DatabaseCleaner.strategy = :transaction
   end
 
-  # start the transaction strategy as examples are run
+  config.before(:each, js: true) do
+    # For future proofing: feature specs usually need truncation
+    DatabaseCleaner.strategy = :deletion
+  end
+
   config.around(:each) do |example|
     DatabaseCleaner.cleaning do
       example.run
     end
   end
-  # [...]
+
+  config.fixture_paths = [Rails.root.join('spec/fixtures')]
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  config.include RequestSpecHelper, type: :request
 end
